33a16a5dcf67d4d1f9eac97331f0e38675b76fde
nameerfarooq <nameerfarooquit@gmail.com>
2023-07-09 17:57:41 +0500

    tutorial ended

--- reduxlearning/src/Shop.jsx
+++ reduxlearning/src/Shop.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { actionCreators } from './store/index'
+import { bindActionCreators } from 'redux'
 const Shop = () => {
+
+  const dispatch = useDispatch()
+  const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch)
   return (
     <div>
-        <div>
-        <button className="btn btn-primary mx-2">-</button>
+      <div>
+        <button className="btn btn-primary mx-2" onClick={() => withdrawMoney(100)} >-</button>
         Add to Cart
-        <button className="btn btn-primary mx-2">+</button>
+        <button className="btn btn-primary mx-2" onClick={() => depositMoney(100)}>+</button>
       </div>
     </div>
   )
